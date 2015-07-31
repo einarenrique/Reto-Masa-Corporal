47d983c0cef3121193b0ea3996180e4fc2cb08e6
Einar Enrique <einarenrique@live.com.mx>
2015-07-31 17:37:18 -0500

    testing 2

--- test.php
+++ test.php
@@ -7,8 +7,11 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 printf($server);
+printf("<br>");
 printf($username);
+printf("<br>");
 printf($password);
+printf("<br>");
 printf($db);
 
 ?>
