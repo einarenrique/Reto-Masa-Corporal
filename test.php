9d55b7eeae99c567b746d06fa19fb8799a296e97
Einar Enrique <einarenrique@live.com.mx>
2015-07-31 17:58:59 -0500

    Prueba heroku

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include ("/fung33ker.php");
+printf("1");
+$funcion=new Funciones();
+printf("2");
+$conexion=$funcion->conectarse();
+printf("3");
+ $sql = "SELECT User FROM usuarios";
+  $tabla=$funcion->busqueda($sql, $conexion);
+  if($tabla[0]!=""){
+    printf($tabla[0]);
+  }
+  else
+  printf("No existe el usuario o la contraseña es incorrecta.");
+?>
