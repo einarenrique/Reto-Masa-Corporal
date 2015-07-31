70a73557ee4541ae4b1154507b8d876152f625bd
Einar Enrique <einarenrique@live.com.mx>
2015-07-31 15:59:05 -0500

    Inicio de sesión
    Archivo PHP donde se comprueba correo y contraseña para iniciar sesión

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include ("fung33ker.php");
+$funcion=new Funciones();
+$conexion=$funcion->conectarse();
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+@$email = $request->email;
+@$pass = $request->pass;
+if(($email != "")&&($pass != "")){;
+  $sql = "SELECT AES_DECRYPT (Password,'$email') FROM usuarios WHERE User = '$email';";
+  $tabla=$funcion->busqueda($sql, $conexion);
+  if($tabla[0]==$pass){
+    $_SESSION['user'] == $email;
+    printf("Ok");
+  }
+  else
+  printf("Error");
+}
+else{
+  printf("Error");
+}
+?>
