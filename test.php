8f69b49dad7f1c976d6f3aa8350f2e8d3cbe34e5
Einar Enrique <einarenrique@live.com.mx>
2015-07-31 17:35:14 -0500

    Testing

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+printf($server);
+printf($username);
+printf($password);
+printf($db);
+
+?>
