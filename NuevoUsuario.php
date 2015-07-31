bce91aebad4224d6b03e19f4f0d7ef683d19b5ec
Einar Enrique <einarenrique@live.com.mx>
2015-07-31 16:19:56 -0500

    Correcciones
    Ahora se comprueba si existe el usuario antes

--- NuevoUsuario.php
+++ NuevoUsuario.php
@@ -8,15 +8,22 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 @$email = $request->email;
 @$pass = $request->pass;
-if(($email != "")&&($pass != "")){;
-  $sql="INSERT INTO usuarios values('','$email',AES_ENCRYPT('$pass','$email'))";
-  $b=$funcion->ejecutar($sql, $conexion);
-  if($b>0){
-    $_SESSION['user'] == $email;
-    printf("Ok");
+if(($email != "")&&($pass != "")){
+  $sql1 = "SELECT User FROM usuarios WHERE User = '$email';";
+  $tabla=$funcion->busqueda($sql1, $conexion);
+  if($tabla[0]==""){
+    $sql="INSERT INTO usuarios values('','$email',AES_ENCRYPT('$pass','$email'))";
+    $b = $funcion->ejecutar($sql, $conexion);
+    if($b > 0){
+      $_SESSION['user'] == $email;
+      printf("Ok");
+    }
+    else printf("Error al agregar a la base de datos.");
   }
-  else
-  printf("Error");
+  else {
+    printf("Ya existe un usuario con ese correo.");
+  }
+
 }
 else{
   printf("Error");
